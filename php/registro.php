1af57597540e2b5c1adc90b8aed9f1f7644f7a73
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 17:50:59 -0400

    registro

--- php/registro.php
+++ php/registro.php
@@ -1,7 +1,11 @@
 <?php
 require_once "db_config.php";
 
-if ($conn->connect_error) die("Conexión fallida: " . $conn->connect_error);
+$conn = new mysqli($host, $db_username, $db_password, $db_name);
+
+if ($conn->connect_error) {
+  die("Conexión fallida: " . $conn->connect_error);
+}
 
 $nombre_usuario = $_POST["nombre_usuario"];
 $contraseña = $_POST["contraseña"];
@@ -15,9 +19,9 @@ if (empty($nombre_usuario) || empty($contraseña) || empty($correo)) {
 
 $contraseña_hash = password_hash($contraseña, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO Usuarios (Nombre_Usuario, Contraseña, Correo_electronico, Rol) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO Usuarios (Nombre_Usuario, Contraseña, Correo_electrónico, Rol) VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssss", $nombre_usuario, $contraseña_hash, $correo, $rol); // 'i' se cambió a 's' para $rol
+$stmt->bind_param("ssss", $nombre_usuario, $contraseña_hash, $correo, $rol);
 
 if ($stmt->execute()) {
   header("Location: ./html/login.html"); // redirige a la página de inicio de sesión después del registro exitoso
