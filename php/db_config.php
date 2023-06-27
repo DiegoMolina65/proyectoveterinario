a1f27242e98fe6e938614e1ec66e9965ad68202a
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 17:56:23 -0400

    db

--- php/db_config.php
+++ php/db_config.php
@@ -1,38 +1,37 @@
 <?php
 
-// Los detalles de la base de datos se definen como constantes
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'veterinario_web');
-define('DB_USER', 'phpmyadmin');
-define('DB_PASSWORD', 'admin');
-define('DB_CHARSET', 'utf8mb4');
+$host = "localhost";
+$db_username = "phpmyadmin";
+$db_password = "admin";
+$db_name = "veterinario_web";
+$db_charset = "utf8mb4";
 
 class Database
 {
     private $host;
-    private $db;
-    private $user;
+    private $username;
     private $password;
+    private $dbname;
     private $charset;
     
-    public function __construct()
+    public function __construct($host, $username, $password, $dbname, $charset)
     {
-        $this->host = DB_HOST;
-        $this->db = DB_NAME;
-        $this->user = DB_USER;
-        $this->password = DB_PASSWORD;
-        $this->charset = DB_CHARSET;
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+        $this->charset = $charset;
     }
 
     public function connect()
     {
         try {
-            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES => false,
             ];
-            $pdo = new PDO($connection, $this->user, $this->password, $options);
+            $pdo = new PDO($dsn, $this->username, $this->password, $options);
 
             // Agregar mensaje emergente para verificar la conexión
             echo "<script>alert('Conexión exitosa a la base de datos');</script>";
@@ -45,9 +44,11 @@ class Database
 }
 
 // Crear una instancia de la clase Database
-$db = new Database();
+$db = new Database($host, $db_username, $db_password, $db_name, $db_charset);
 // Establecer conexión con la base de datos
 $conn = $db->connect();
 
 // Asegúrate de que el objeto de conexión esté disponible en otros archivos
 // Puedes usar $conn en otros archivos incluyendo este archivo
+
+?>
