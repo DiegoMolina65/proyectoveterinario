a5c1f2ff138aa452846f175f79b7c6924f6ff7c6
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 23:36:25 -0400

    new file:   html/registromascota.php

---
+++ html/registromascota.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro de Mascota</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Registro de Mascota</h1>
+    <h2>Registrar Mascota</h2>
+    <form action="crud_mascota.php" method="post">
+        <input type="hidden" name="action" value="create">
+        <label for="id_cliente">ID del Cliente:</label><br>
+        <input type="text" id="id_cliente" name="id_cliente"><br>
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" id="nombre" name="nombre"><br>
+        <label for="especie">Especie:</label><br>
+        <input type="text" id="especie" name="especie"><br>
+        <label for="raza">Raza:</label><br>
+        <input type="text" id="raza" name="raza"><br>
+        <label for="fecha_nacimiento">Fecha de Nacimiento:</label><br>
+        <input type="date" id="fecha_nacimiento" name="fecha_nacimiento"><br>
+        <label for="peso">Peso:</label><br>
+        <input type="text" id="peso" name="peso"><br>
+        <label for="color">Color:</label><br>
+        <input type="text" id="color" name="color"><br>
+        <label for="historial_medico">Historial Médico:</label><br>
+        <textarea id="historial_medico" name="historial_medico"></textarea><br>
+        <input type="submit" value="Registrar">
+    </form>
+
+    <h2>Datos del Dueño</h2>
+    <form action="crud_mascota.php" method="post">
+        <input type="hidden" name="action" value="get_owner">
+        <label for="id_cliente_dueno">ID del Cliente:</label><br>
+        <input type="text" id="id_cliente_dueno" name="id_cliente_dueno"><br>
+        <input type="submit" value="Mostrar Datos del Dueño">
+    </form>
+
+    <h2>Tabla de Mascotas Registradas</h2>
+    <table>
+        <tr>
+            <th>ID Mascota</th>
+            <th>ID Cliente</th>
+            <th>Nombre</th>
+            <th>Especie</th>
+            <th>Raza</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Peso</th>
+            <th>Color</th>
+            <th>Historial Médico</th>
+            <th>Acciones</th>
+        </tr>
+        <?php foreach($mascotas as $mascota): ?>
+        <tr>
+            <td><?php echo $mascota["ID_Mascota"]; ?></td>
+            <td><?php echo $mascota["ID_Cliente"]; ?></td>
+            <td><?php echo $mascota["Nombre"]; ?></td>
+            <td><?php echo $mascota["Especie"]; ?></td>
+            <td><?php echo $mascota["Raza"]; ?></td>
+            <td><?php echo $mascota["Fecha_Nacimiento"]; ?></td>
+            <td><?php echo $mascota["Peso"]; ?></td>
+            <td><?php echo $mascota["Color"]; ?></td>
+            <td><?php echo $mascota["Historial_Medico"]; ?></td>
+            <td>
+                <form action="crud_mascota.php" method="post">
+                    <input type="hidden" name="action" value="delete">
+                    <input type="hidden" name="id_mascota" value="<?php echo $mascota["ID_Mascota"]; ?>">
+                    <button type="submit">Eliminar</button>
+                </form>
+                <form action="crud_mascota.php" method="post">
+                    <input type="hidden" name="action" value="update">
+                    <input type="hidden" name="id_mascota" value="<?php echo $mascota["ID_Mascota"]; ?>">
+                    <button type="submit">Actualizar</button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
---
+++ php/crud_mascota.php
@@ -0,0 +1,83 @@
+<?php
+require_once "db_config.php";
+
+$action = $_POST["action"];
+
+if ($action == "create") {
+    $id_cliente = $_POST["id_cliente"];
+    $nombre = $_POST["nombre"];
+    $especie = $_POST["especie"];
+    $raza = $_POST["raza"];
+    $fecha_nacimiento = $_POST["fecha_nacimiento"];
+    $peso = $_POST["peso"];
+    $color = $_POST["color"];
+    $historial_medico = $_POST["historial_medico"];
+
+    $sql = "INSERT INTO Mascotas (ID_Cliente, Nombre, Especie, Raza, Fecha_Nacimiento, Peso, Color, Historial_Medico) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("isssssss", $id_cliente, $nombre, $especie, $raza, $fecha_nacimiento, $peso, $color, $historial_medico);
+    if($stmt->execute()){
+        header("Location: registromascota.php");
+        exit();
+    } else {
+        echo "Error al registrar mascota: " . $stmt->error;
+    }
+} elseif ($action == "get_owner") {
+    $id_cliente_dueno = $_POST["id_cliente_dueno"];
+
+    $sql = "SELECT * FROM Clientes WHERE ID_Cliente = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id_cliente_dueno);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $cliente = $result->fetch_assoc();
+    
+    if ($cliente) {
+        echo "<h3>Datos del Dueño</h3>";
+        echo "<p>Nombre: " . $cliente['Nombre'] . "</p>";
+        echo "<p>Apellido: " . $cliente['Apellido'] . "</p>";
+        echo "<p>Dirección: " . $cliente['Dirección'] . "</p>";
+        echo "<p>Ciudad: " . $cliente['Ciudad'] . "</p>";
+        echo "<p>Teléfono: " . $cliente['Teléfono'] . "</p>";
+        echo "<p>Correo Electrónico: " . $cliente['Correo_electrónico'] . "</p>";
+    } else {
+        echo "No se encontró un cliente con ese ID.";
+    }
+} elseif ($action == "update") {
+    $id_mascota = $_POST["id_mascota"];
+    $nombre = $_POST["nombre"];
+    $especie = $_POST["especie"];
+    $raza = $_POST["raza"];
+    $fecha_nacimiento = $_POST["fecha_nacimiento"];
+    $peso = $_POST["peso"];
+    $color = $_POST["color"];
+    $historial_medico = $_POST["historial_medico"];
+
+    $sql = "UPDATE Mascotas SET Nombre=?, Especie=?, Raza=?, Fecha_Nacimiento=?, Peso=?, Color=?, Historial_Medico=? WHERE ID_Mascota=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssssi", $nombre, $especie, $raza, $fecha_nacimiento, $peso, $color, $historial_medico, $id_mascota);
+    if($stmt->execute()){
+        header("Location: registromascota.php");
+        exit();
+    } else {
+        echo "Error al actualizar mascota: " . $stmt->error;
+    }
+} elseif ($action == "delete") {
+    $id_mascota = $_POST["id_mascota"];
+
+    $sql = "DELETE FROM Mascotas WHERE ID_Mascota = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id_mascota);
+    if($stmt->execute()){
+        header("Location: registromascota.php");
+        exit();
+    } else {
+        echo "Error al eliminar mascota: " . $stmt->error;
+    }
+} else {
+    echo "Acción no válida.";
+}
+
+$stmt->close();
+$conn->close();
+?>
