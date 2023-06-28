a9ce6420c8ec8ce257609a70519bb5699a03656c
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 22:40:10 -0400

    modified:   html/registrocliente.php

--- html/registrocliente.php
+++ html/registrocliente.php
@@ -6,28 +6,11 @@
 </head>
 <body>
     <h1>Registro de Cliente</h1>
-    <form action="../php/crud_clientes.php" method="post">
-        <input type="hidden" name="action" value="create">
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre"><br>
-        <label for="apellido">Apellido:</label><br>
-        <input type="text" id="apellido" name="apellido"><br>
-        <label for="direccion">Dirección:</label><br>
-        <input type="text" id="direccion" name="direccion"><br>
-        <label for="ciudad">Ciudad:</label><br>
-        <input type="text" id="ciudad" name="ciudad"><br>
-        <label for="telefono">Teléfono:</label><br>
-        <input type="text" id="telefono" name="telefono"><br>
-        <label for="correo">Correo Electrónico:</label><br>
-        <input type="text" id="correo" name="correo"><br>
-        <input type="submit" value="Registrar">
-    </form>
-    
-    <h2>Lista de Clientes</h2>
     <?php
         session_start();
         $clientes = $_SESSION['clientes'] ?? [];
     ?>
+    <h2>Lista de Clientes</h2>
     <table>
         <tr>
             <th>ID</th>
@@ -52,6 +35,12 @@
                 <form action="../php/crud_clientes.php" method="post">
                     <input type="hidden" name="action" value="update">
                     <input type="hidden" name="id_cliente" value="<?php echo $cliente["ID_Cliente"]; ?>">
+                    <input type="text" name="nombre" value="<?php echo $cliente["Nombre"]; ?>">
+                    <input type="text" name="apellido" value="<?php echo $cliente["Apellido"]; ?>">
+                    <input type="text" name="direccion" value="<?php echo $cliente["Dirección"]; ?>">
+                    <input type="text" name="ciudad" value="<?php echo $cliente["Ciudad"]; ?>">
+                    <input type="text" name="telefono" value="<?php echo $cliente["Teléfono"]; ?>">
+                    <input type="text" name="correo" value="<?php echo $cliente["Correo_electrónico"]; ?>">
                     <input type="submit" value="Actualizar">
                 </form>
                 <form action="../php/crud_clientes.php" method="post">
@@ -64,11 +53,22 @@
         <?php endforeach; ?>
     </table>
     
-    <script>
-        // Actualizar la página después de enviar el formulario de registro
-        if (window.history.replaceState) {
-            window.history.replaceState(null, null, window.location.href);
-        }
-    </script>
+    <h2>Registrar Cliente</h2>
+    <form action="../php/crud_clientes.php" method="post">
+        <input type="hidden" name="action" value="create">
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" id="nombre" name="nombre"><br>
+        <label for="apellido">Apellido:</label><br>
+        <input type="text" id="apellido" name="apellido"><br>
+        <label for="direccion">Dirección:</label><br>
+        <input type="text" id="direccion" name="direccion"><br>
+        <label for="ciudad">Ciudad:</label><br>
+        <input type="text" id="ciudad" name="ciudad"><br>
+        <label for="telefono">Teléfono:</label><br>
+        <input type="text" id="telefono" name="telefono"><br>
+        <label for="correo">Correo Electrónico:</label><br>
+        <input type="text" id="correo" name="correo"><br>
+        <input type="submit" value="Registrar">
+    </form>
 </body>
 </html>
