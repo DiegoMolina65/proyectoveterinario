2d750997540872a763f1fea0446f87faee9c7b4e
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 21:50:39 -0400

    modified:   html/registro.html 	modified:   php/login.php

--- php/login.php
+++ php/login.php
@@ -10,20 +10,28 @@ if (empty($nombre_usuario) || empty($contraseña)) {
   exit();
 }
 
-$sql = "SELECT Contraseña FROM Usuarios WHERE Nombre_Usuario='$nombre_usuario'";
-$result = $conn->query($sql);
+$sql = "SELECT Contraseña FROM Usuarios WHERE Nombre_Usuario=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $nombre_usuario);
 
-if ($result->num_rows > 0) {
-  $row = $result->fetch_assoc();
-  if (password_verify($contraseña, $row["Contraseña"])) {
-    header("Location: ../html/info.html");;
-    exit();
+if ($stmt->execute()) {
+  $result = $stmt->get_result();
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if (password_verify($contraseña, $row["Contraseña"])) {
+      header("Location: ../html/info.html");
+      exit();
+    } else {
+      echo "Contraseña incorrecta.";
+    }
   } else {
-    echo "Contraseña incorrecta.";
+    echo "No existe un usuario con ese nombre de usuario.";
   }
 } else {
-  echo "No existe un usuario con ese nombre de usuario.";
+  echo "Error: " . $conn->error;
 }
 
+$stmt->close();
 $conn->close();
+
 ?>
