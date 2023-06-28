67624675f6b3cff9df0ae7852ded75b73ee84ef1
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 22:01:56 -0400

    modified:   database.sql 	modified:   html/info.html 	modified:   php/login.php

--- php/login.php
+++ php/login.php
@@ -1,37 +1,43 @@
 <?php
+session_start();
+
 require_once 'db_config.php';
 
-$nombre_usuario = $_POST["nombre_usuario"];
-$contraseña = $_POST["contraseña"];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['nombre_usuario']) && !empty($_POST['contraseña'])) {
+        $nombre_usuario = $_POST['nombre_usuario'];
+        $contraseña = $_POST['contraseña'];
 
-// Verificación de datos vacíos
-if (empty($nombre_usuario) || empty($contraseña)) {
-  echo "Por favor, complete todos los campos.";
-  exit();
-}
+        $sql = "SELECT Contraseña FROM Usuarios WHERE Nombre_Usuario = ?";
+        $stmt = $conn->prepare($sql);
 
-$sql = "SELECT Contraseña FROM Usuarios WHERE Nombre_Usuario=?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $nombre_usuario);
+        if ($stmt === false) {
+            die('Error en la preparación: ' . htmlspecialchars($conn->error));
+        }
 
-if ($stmt->execute()) {
-  $result = $stmt->get_result();
-  if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    if (password_verify($contraseña, $row["Contraseña"])) {
-      header("Location: ../html/info.html");
-      exit();
+        $stmt->bind_param('s', $nombre_usuario);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            if (password_verify($contraseña, $row['Contraseña'])) {
+                $_SESSION['loggedin'] = true;
+                $_SESSION['nombre_usuario'] = $nombre_usuario;
+                header("Location: ../html/info.html");
+                exit();
+            } else {
+                echo 'Contraseña incorrecta.';
+            }
+        } else {
+            echo 'No existe un usuario con ese nombre de usuario.';
+        }
+
+        $stmt->close();
     } else {
-      echo "Contraseña incorrecta.";
+        echo 'Por favor, complete todos los campos.';
     }
-  } else {
-    echo "No existe un usuario con ese nombre de usuario.";
-  }
-} else {
-  echo "Error: " . $conn->error;
 }
 
-$stmt->close();
 $conn->close();
-
 ?>
