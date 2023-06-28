2dda91b2b17dc1c4e7a47407c55e3631c214cad5
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 21:13:29 -0400

    modified:   php/login.php 	modified:   php/registro.php

--- php/login.php
+++ php/login.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   $row = $result->fetch_assoc();
   if (password_verify($contraseña, $row["Contraseña"])) {
-    header("Location: /proyectoveterinario/html/info.html");;
+    header("Location: ../proyectoveterinario/html/info.html");;
     exit();
   } else {
     echo "Contraseña incorrecta.";
