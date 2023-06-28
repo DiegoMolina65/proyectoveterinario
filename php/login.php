cbbe5cacaf7426f1c2022beb36c7e47e4efc5400
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 22:09:10 -0400

    modified:   php/login.php

--- php/login.php
+++ php/login.php
@@ -11,22 +11,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $nombre_usuario = $_POST['nombre_usuario'];
         $contraseña = $_POST['contraseña'];
 
-        $sql = "SELECT Contraseña FROM Usuarios WHERE Nombre_Usuario = ?";
+        $sql = "SELECT Contraseña FROM Usuarios WHERE Nombre_Usuario = :nombre_usuario";
         $stmt = $conn->prepare($sql);
 
         if ($stmt === false) {
-            die('Error en la preparación: ' . htmlspecialchars($conn->error));
+            die('Error en la preparación: ' . htmlspecialchars($conn->errorInfo()));
         }
 
-        $stmt->bind_param('s', $nombre_usuario);
+        $stmt->bindParam(':nombre_usuario', $nombre_usuario);
         $stmt->execute();
 
-        $result = $stmt->get_result();
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            if (password_verify($contraseña, $row['Contraseña'])) {
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            if (password_verify($contraseña, $result['Contraseña'])) {
                 $_SESSION['loggedin'] = true;
                 $_SESSION['nombre_usuario'] = $nombre_usuario;
+                session_write_close();
                 header("Location: ../html/info.html");
                 exit();
             } else {
@@ -35,12 +35,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         } else {
             echo 'No existe un usuario con ese nombre de usuario.';
         }
-
-        $stmt->close();
     } else {
         echo 'Por favor, complete todos los campos.';
     }
 }
 
-$conn->close();
+$conn = null;
 ?>
