ee6a55cf48a352e246a5072cb7c4ed34ea8c29aa
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-28 00:10:23 -0400

    modified:   html/registromascota.php

--- html/registromascota.php
+++ html/registromascota.php
@@ -19,7 +19,7 @@ $conn->close();
 <head>
     <title>Registro de Mascota</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../css/stylecrudmascota.css">
 </head>
 <body>
     <h1>Registro de Mascota</h1>
