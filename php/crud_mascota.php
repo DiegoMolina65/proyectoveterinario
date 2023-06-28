f867b478b303751aef90ca7d0e763c8ef9468c62
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-28 09:37:53 -0400

    modified:   php/crud_mascota.php

--- php/crud_mascota.php
+++ php/crud_mascota.php
@@ -55,20 +55,29 @@ if ($action == "create") {
     } else {
         echo "Error al eliminar mascota: " . $stmt->errorInfo()[2];
     }
-} else if ($action == "search") {
-    $id_cliente = $_GET["id_cliente"];
+}   else if ($action == "search") {
+        $id_cliente = $_GET["id_cliente"];
+        
+        // Recupera la información del cliente
+        $sql_cliente = "SELECT * FROM Clientes WHERE ID_Cliente = ?";
+        $stmt_cliente = $conn->prepare($sql_cliente);
+        $stmt_cliente->execute([$id_cliente]);
+        $cliente = $stmt_cliente->fetch(PDO::FETCH_ASSOC);
+        
+        // Recupera las mascotas de este cliente
+        $sql_mascotas = "SELECT * FROM Mascotas WHERE ID_Cliente = ?";
+        $stmt_mascotas = $conn->prepare($sql_mascotas);
+        $stmt_mascotas->execute([$id_cliente]);
+        $mascotas = $stmt_mascotas->fetchAll(PDO::FETCH_ASSOC);
+        
+        // Guarda el cliente y sus mascotas en variables de sesión para su uso en la página de registro de mascotas
+        session_start();
+        $_SESSION['cliente'] = $cliente;
+        $_SESSION['mascotas'] = $mascotas;
+        header('Location: ../html/registrocliente.php');
+        exit();
+    }
     
-    $sql = "SELECT * FROM Mascotas WHERE ID_Cliente = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$id_cliente]);
-    $mascotas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Guardar las mascotas en una variable de sesión para su uso en la página de registro de mascotas.
-    session_start();
-    $_SESSION['mascotas'] = $mascotas;
-    header('Location: ../html/registromascota.php');
-    exit();
-}
 
 $stmt = null;
 $conn = null;
