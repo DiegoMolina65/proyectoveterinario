2893a5be3ee502555b37af3b89d9658ec65131da
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-27 22:47:50 -0400

    new file:   css/stylescrudcliente.css 	modified:   html/registrocliente.php

--- html/registrocliente.php
+++ html/registrocliente.php
@@ -3,6 +3,7 @@
 <head>
     <title>Registro de Cliente</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/stylescrudcliente.css">
 </head>
 <body>
     <h1>Registro de Cliente</h1>
